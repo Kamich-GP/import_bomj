6cb2dcdc4494f6785da4a9b7dd73df2b1bf8c9b1
Kamich-GP <yurochkagameplus@gmail.com>
2025-02-04 21:44:47 +0500

    buttons

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ def start(message):
     # Проверяем наличие пользователя
     if database.check_user(user_id):
         bot.send_message(user_id, 'Добро пожаловать!')
+        bot.send_message(user_id, 'Выберите пункт меню:',
+                         reply_markup=buttons.main_menu(database.get_pr_buttons()))
     else:
         bot.send_message(user_id, 'Привет! Давай начнем регистрацию!\n'
                                   'Напиши свое имя!', reply_markup=telebot.types.ReplyKeyboardRemove())
@@ -45,6 +47,19 @@ def get_num(message, user_name):
         bot.send_message(user_id, 'Отправьте номер через кнопку!')
         # Возврат на этап получения номера
         bot.register_next_step_handler(message, get_num, user_name)
+
+
+# Выбор товара
+@bot.callback_query_handler(lambda call: int(call.data) in [i[0] for i in database.get_all_pr()])
+def choose_product(call):
+    user_id = call.message.chat.id
+    pr_info = database.get_exact_pr(int(call.data))
+    bot.delete_message(chat_id=user_id, message_id=call.message.message_id)
+    bot.send_photo(user_id, photo=pr_info[5], caption=f'{pr_info[1]}\n\n'
+                                                      f'Описание: {pr_info[2]}\n'
+                                                      f'Количество: {pr_info[3]}\n'
+                                                      f'Цена: ${pr_info[4]}',
+                   reply_markup=buttons.choose_pr_count(pr_info[3]))
 
 
 # Запуск бота
